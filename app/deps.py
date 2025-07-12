0f3af368bb3c74ae7bd9f6035610691cd8852212
Aleksandr Rodio <rodionovlead@gmail.com>
2025-07-12 13:17:31 +0300

    Update deps.py

--- app/deps.py
+++ app/deps.py
@@ -1,1 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./db.sqlite3")
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
